2d0d92fbf9756084405a9568298a9b82e07b98b0
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-06-11 23:54:34 +0100

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react-scroll'],
-    },
-  },
-  resolve: {
-    alias: {
-      'react-scroll': 'react-scroll/modules',
-    },
-  },
 })
